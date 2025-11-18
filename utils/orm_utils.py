ff44cb8535530ada1294509452b9423915bb8df5
Robyn Felicity Heather Veitch <robynfhveitch@gnail.com>
2025-11-18 12:27:14 +0000

    fix: dialect check in 'default_uid' function was returning the entire Dialect object, not the dialect name meaning failures on sqlite and postgresql

--- utils/orm_utils.py
+++ utils/orm_utils.py
@@ -3,11 +3,15 @@ from uuid import uuid4 as uuid
 from sqlalchemy import text
 from sqlalchemy.engine.default import DefaultExecutionContext
 
+
 def default_uuid(context: DefaultExecutionContext):
-    '''
+    """
     Function to be used exclusively in Column 'default' attributes.
     Returns a `sys_guid` call for Oracle-like systems but returns a more generic UUID for SQLite.
-    '''
-    if context.engine.dialect == 'sqlite':
+    """
+    if (
+        context.engine.dialect.name == 'sqlite'
+        or context.engine.dialect.name == 'postgresql'
+    ):
         return uuid().bytes
     return text('SYS_GUID()')
